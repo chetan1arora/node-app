5f1a94f410c1630f4b801a56c8c1d232c5bb8caf
lolCod3r <chetan.arora02@gmail.com>
2020-07-21 18:23:33 +0530

    Separating main process and ui

--- main.go
+++ main.go
@@ -2,14 +2,13 @@ package main
 
 import(
 "./pkg"
-"fmt"
-"time"
+"syscall"
+"os"
 )
 
 /*
-	Main Node Application
+ Greeting
 */
-
 func showGreeting(){
 	fmt.Println("|----------------------------------------------------|")
 	fmt.Println("*------------------____---_______---__---------___---*")
@@ -27,7 +26,50 @@ func showGreeting(){
 	fmt.Println("[4] Show info")
 	return
 }
+/*
+ Handle Error
+*/
+func handleErr(err error){
+	if err != nil {
+		panic(err)
+	}
+}
 
+var pipeWrite = "/go/pipe/read"
+var pipeRead = "/go/pipe/write"
+
+func waitForTerminal(){
+	err := os.Mkdir("/go/pipe",0666)	
+	handleErr(err)
+	err = syscall.Mkfifo(pipeWrite, 0666)
+	handleErr(err)
+	err = syscall.Mkfifo(pipeRead, 0666)
+	handleErr(err)
+	// to open pipe to write    
+	readFd, err := syscall.Open(pipeRead, syscall.O_RDONLY| syscall.O_CREAT, 755)
+	handleErr(err)
+	//to open pipe to read    
+	writeFd,err = syscall.Open(pipeWrite, syscall.O_WRONLY| syscall.O_CREAT, 755)
+	handleErr(err)
+	// Wait for client terminal before closing process fds
+	temp := make([]byte,10)
+	_, err = syscall.Read(readFd,temp)
+	handleErr(err)
+	// Replace process fds with remote processes
+	err = syscall.Close(0) // STDIN
+	handleErr(err)
+	err = syscall.Close(1) // STDOUT
+	handleErr(err)
+	err = syscall.Dup2(readFd,0)
+	handleErr(err)
+	err = syscall.Dup2(writeFd,1)
+	handleErr(err)
+	return
+}
+
+/*
+	Main Node Application
+*/
 func main(){
 	var myNode base.Node
 
@@ -41,13 +83,14 @@ func main(){
 	go myNode.ListenerLoop()
 
 	// Join Society
-	// go myNode.JoinSociety()
+	go myNode.JoinSociety()
+
+	// Wait for terminal
+	waitForTerminal()
 
 	// Main Purpose
 	for {
 		showGreeting()
-		// Testing 
-		time.Sleep(25*time.Second)
 		var op int
 		fmt.Scanln(&op)
 		switch op {
@@ -62,6 +105,5 @@ func main(){
 			break
 		}
 	}
-
 	return
 }
